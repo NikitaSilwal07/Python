f830545fbe3a0cebb99d5a17554ef84d7ab8c760
Nikita Silwal <silwalnikita@gmail.com>
2024-10-02 21:49:11 +0545

    changes

--- string.py/qn17.py
+++ string.py/qn17.py
@@ -1,4 +1,4 @@
 # 17. Write a Python program that replaces all occurrences of a substring within a string with another substring.
 
-string="He lives is Bhaktapur. He codes is VSCode."
+string="She lives is Kathmandu. He codes is VSCode."
 print(string.replace("is","in"))
